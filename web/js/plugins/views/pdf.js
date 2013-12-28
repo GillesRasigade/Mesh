a09094af463f81fab9ee54a67c329c7fb10084e4
billou <gilles.rasigade+github@gmail.com>
2013-12-28 04:46:26 +0100

    PDF URL fixed on window resize

--- web/js/plugins/views/pdf.js
+++ web/js/plugins/views/pdf.js
@@ -10,7 +10,8 @@
             
                 src: function ( path , mode ) {
                     return $m.api.utils.url('pdf','access',{
-                        path: path, mode: mode !== undefined ? mode : 'full'
+                        path: path, mode: mode !== undefined ? mode : 'full',
+                        shared: true
                     });
                 },
             
