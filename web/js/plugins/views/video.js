9c0182b0a75b15a78eb0f63508ac01dbfa532931
billou <gilles.rasigade+github@gmail.com>
2013-12-31 14:16:50 +0100

    Shared resource fixed

--- web/js/plugins/views/video.js
+++ web/js/plugins/views/video.js
@@ -11,7 +11,7 @@
                 src: function ( path , mode ) {
                     return $m.api.utils.url( mode == 'thumb' ? 'video' : 'file' , mode == 'thumb' ? 'access' : 'access' , {
                         path: path, mode: mode !== undefined ? mode : 'full',
-                        shared: true
+                        shared: !$m.shared
                     });
                 }, 
             
