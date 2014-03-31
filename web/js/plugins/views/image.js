dc5a64be1228cf8db993cc6c4b5df610faa9a007
billou <gilles.rasigade+github@gmail.com>
2014-03-31 20:38:43 +0200

    Update image.js

--- web/js/plugins/views/image.js
+++ web/js/plugins/views/image.js
@@ -135,7 +135,7 @@
                                     
                                     (function(p,c){
                                         var image = new Image();
-                                        var $div = $('<div class="image entry" data-path="'+p+'" title="'+p.replace(/.*\//,'')+'" data-toggle="tooltip"></div>');
+                                        var $div = $('<div class="image entry" data-path="'+p+'" data-toggle="tooltip"></div>');
 
                                         var $img = $('<img draggable="true" data-preview="'+$m.view.image.src(p,'preview')+'" src="'+$m.view.image.src(p,'thumb')+'" style="width: 100%; display: none;"/>');
 
